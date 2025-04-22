8e8e93c756ccabe1131fb9fdb4aa500a1c49c478
ali-al-ismail <2220005098@iau.edu.sa>
2025-04-22 18:29:16 +0300

    crypto: miller rabin implemented

--- src/crypto/rsa.py
+++ src/crypto/rsa.py
@@ -9,8 +9,9 @@
 # - digital signature signing and verification
 ###############################################################
 
+import secrets
 from dataclasses import dataclass
-from random import randint
+
 
 @dataclass
 class PublicKey:
@@ -18,7 +19,7 @@ class PublicKey:
 
     n: int
     e: int
-    
+
 @dataclass
 class PrivateKey:
     """Private key class."""
@@ -37,32 +38,58 @@ class RSA:
     def _generate_prime(length: int) -> int:
         """Generate a prime number of given length."""
         return 1
-    
-    
+
+
     @staticmethod
-    def _miller_rabin(n: int, k: int) -> bool:
-        """Miller-Rabin primality test.
+    def _miller_rabin(n: int, acc: int) -> bool:
+        """
+        Miller-Rabin primality test.
 
         Args:
             n (int): Number to test for primality.
-            k (int): Number of iterations.
+            acc (int): Number of iterations.
 
         Returns:
             bool: True if n is prime, False otherwise.
-        """
 
-       
+        """
+        if n <= 1:
+            return False
+        if n <= 3:  # noqa: PLR2004
+            return True
+        if n % 2 == 0:
+            return False
+        r = 0
+        d = n - 1
+        while d % 2 == 0:
+            d //= 2
+            r += 1
+
+        for _ in range(acc):
+            a = secrets.randbelow(n - 4) + 2
+            x = pow(a, d, n)
+
+            if x == 1 or x == n - 1:
+                continue
+
+            for _ in range(r - 1):
+                x = pow(x, 2, n)
+                if x == n - 1:
+                    break
+            else:
+                return False
+
         return True
-        
-        
+
+
 
     def generate_keys(self) -> None:
         pass
 
     def encrypt(self, plaintext: str, public_key: PublicKey) -> str:
         """Encrypt the plaintext using the public key."""
-        return ''
+        return ""
 
     def decrypt(self, ciphertext: str, private_key: PrivateKey) -> str:
         """Decrypt the ciphertext using the private key."""
-        return ''
+        return ""
