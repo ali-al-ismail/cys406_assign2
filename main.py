5ba453cffd6912466288123b02feca98924bfc68
ali-al-ismail <2220005098@iau.edu.sa>
2025-04-22 17:17:26 +0300

    initial components

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cys406-assign2!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/__main__.py
@@ -0,0 +1,8 @@
+"""Application entry point."""
+
+def main():
+    print("Hello from cys-406-assign2!")
+
+
+if __name__ == "__main__":
+    main()
